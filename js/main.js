56989ba72750c4fb59eb9463c7352cc3c244430c
Damian Krzak <damian.w.krzak@gmail.com>
2022-03-30 14:07:19 +0200

    added section recomended-islands

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const navBtn = document.querySelector(".navigation__burger");
 const navigation = document.querySelector(".navigation__list");
+const cardsBtn = document.querySelectorAll(".recomended-islands__btn");
+const back = document.querySelectorAll(".recomended-islands__back");
+const front = document.querySelectorAll(".recomended-islands__front");
 
 const handlerNavigation = () => {
   navigation.classList.toggle("navigation__list--active");
