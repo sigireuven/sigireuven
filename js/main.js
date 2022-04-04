67bd94effe248b1bcf087be05e382d70933900db
Damian Krzak <damian.w.krzak@gmail.com>
2022-04-04 14:16:23 +0200

    added section opinions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
 const navBtn = document.querySelector(".navigation__burger");
 const navigation = document.querySelector(".navigation__list");
-const cardsBtn = document.querySelectorAll(".recomended-islands__btn");
-const back = document.querySelectorAll(".recomended-islands__back");
-const front = document.querySelectorAll(".recomended-islands__front");
+const opinions = document.querySelectorAll(".opinions__opinion");
+const next = document.querySelector(".opinions__next");
+const previous = document.querySelector(".opinions__previous");
+let index = 0;
+const hanlerNextOpinion = () => {
+  for (let i = 0; i < opinions.length; i++) {
+    opinions[i].classList.remove("opinions--active");
+  }
+  index++;
+  if (index === opinions.length) {
+    index = 0;
+  }
+  opinions[index].classList.add("opinions--active");
+};
+const hanlerPreviousOpinion = () => {
+  for (let i = 0; i < opinions.length; i++) {
+    opinions[i].classList.remove("opinions--active");
+  }
+  index--;
+  if (index < 0) {
+    index = opinions.length - 1;
+  }
+  opinions[index].classList.add("opinions--active");
+};
 
 const handlerNavigation = () => {
   navigation.classList.toggle("navigation__list--active");
 };
 
 navBtn.addEventListener("click", handlerNavigation);
+next.addEventListener("click", hanlerNextOpinion);
+previous.addEventListener("click", hanlerPreviousOpinion);
